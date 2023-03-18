9f10e602007d1dc712a49c291ce5ba61dd7c1922
aaditpj <aaditjaveri@yahoo.co.in>
2023-03-18 15:37:32 -0400

    added the distribution for duration
    Had the change the format of one row from char to time class to help with easier processing.

--- Data_Science_Project.R
+++ Data_Science_Project.R
@@ -1,8 +1,11 @@
 library(tidyverse)
 library(modelr)
+library(lubridate)
 
 economy_data = read_csv("Flights Price Prediction Dataset\\economy.csv", show_col_types = FALSE)
 business_data = read_csv("Flights Price Prediction Dataset\\business.csv", show_col_types = FALSE)
+economy_data$time_taken = lubridate::hm(economy_data$time_taken)
+business_data$time_taken = lubridate::hm(business_data$time_taken)
 head(economy_data)
 head(business_data)
 
@@ -14,4 +17,13 @@ ggplot (data = economy_data) +
 ggplot (data = business_data) +
   geom_histogram(mapping = aes(x=dep_time))
 
+
+#distribution with respect to duration
+
+
+ggplot (data = economy_data) +
+  geom_histogram(mapping = aes(x=(lubridate::as.duration(time_taken)/3600)))
+
+ggplot (data = business_data) +
+  geom_histogram(mapping = aes(x=(lubridate::as.duration(time_taken)/3600)))
 
