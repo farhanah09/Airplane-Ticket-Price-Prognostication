0be75a1d9f6f167749d3a85953ecc1b511b56f52
aaditpj <aaditjaveri@yahoo.co.in>
2023-03-18 15:18:59 -0400

    Added the code to plot distribution with respect to time of departure in the day

--- Data_Science_Project.R
+++ Data_Science_Project.R
@@ -6,3 +6,12 @@ business_data = read_csv("Flights Price Prediction Dataset\\business.csv", show_
 head(economy_data)
 head(business_data)
 
+#distribution with respect to departure time
+
+ggplot (data = economy_data) +
+  geom_histogram(mapping = aes(x=dep_time))
+
+ggplot (data = business_data) +
+  geom_histogram(mapping = aes(x=dep_time))
+
+
