3bdda9c87bd0bbc34861ffc77defc01403a4f1cc
AarohiK <62942065+aarkap@users.noreply.github.com>
2023-03-19 18:44:36 -0400

    Update Data_Science_Project.R
    Price distribution based on weekday

--- Data_Science_Project.R
+++ Data_Science_Project.R
@@ -27,3 +27,22 @@ ggplot (data = economy_data) +
 ggplot (data = business_data) +
   geom_histogram(mapping = aes(x=(lubridate::as.duration(time_taken)/3600)))
 
+
+#Price distribution based on the day of the week
+  #Converting date to weekday
+economy_data$date <- as.Date(economy_data$date)
+economy_data$weekday <- strftime(economy_data$date,"%A") 
+business_data$date <- as.Date(business_data$date)
+business_data$weekday <- strftime(business_data$date,"%A")
+  #Plotting price vs day of economy
+weekday_graph_economy <- ggplot (data = economy_data) +
+  geom_point(mapping = aes (x = weekday, y = price))+
+  geom_point(mapping = aes (x = weekday, y = median(price)),color="red")+
+  geom_point(mapping = aes (x = weekday, y = mean(price)),color="gold2")
+  #Plotting price vs day of business
+weekday_graph_business <- ggplot (data = business_data) +
+  geom_point(mapping = aes (x = weekday, y = price))+
+  geom_point(mapping = aes (x = weekday, y = median(price)),color="red")+
+  geom_point(mapping = aes (x = weekday, y = mean(price)),color="gold2")
+
+
