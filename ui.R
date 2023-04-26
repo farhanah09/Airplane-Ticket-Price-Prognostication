ea1a49a129462d2619da67cf43b247ba76e53c72
Farhan Ahmad <farhanah@andrew.cmu.edu>
2023-04-26 12:24:57 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,17 +11,17 @@ library(pROC)
 library(caTools)
 library(randomForest)
 
-setwd("c:\\Users\\Aadit Javeri\\Desktop\\CMU Stuff\\Data Science for Technology - 19603\\DS_Project\\Data_Science_Project_R")
+#setwd("\\andrew.ad.cmu.edu/users/users23/farhanah/Documents/GitHub")
 
 flightPricePredict <- function(Airline, Source, Destination, Flight_date, Days_left, Class, Total_stops, Arrival, Departure){
-  flight_new = read_csv("Flights Price Prediction Dataset\\Cleaned_dataset.csv", show_col_types = FALSE)
+  flight_new = read_csv("~/GitHub/Data_Science_Project_R/Flights Price Prediction Dataset/Cleaned_dataset.csv", show_col_types = FALSE)
   unique(flight_new$Total_stops)
   flight_new["Total_stops"][flight_new["Total_stops"]== "non-stop"] <- '0'
   flight_new["Total_stops"][flight_new["Total_stops"]== "1-stop"] <- '1'
   flight_new["Total_stops"][flight_new["Total_stops"]== "2+-stop"] <- '2'
   flight_new$Total_stops = as.numeric(flight_new$Total_stops)
   
-  model <- readRDS("model_file.rds")
+  model <- readRDS("~/GitHub/Data_Science_Project_R/model_file.rds")
   Journey_day = strftime(Flight_date,"%A")
   flight_inpt = data.frame(Journey_day, Airline, Class, Source, Departure, Total_stops, Arrival, Destination, Days_left)
   inp = model.matrix( ~ ., data =rbind(flight_inpt, select(flight_new,-Fare, -Flight_code, -Date_of_journey, -Duration_in_hours)))
@@ -33,7 +33,6 @@ flightPricePredict <- function(Airline, Source, Destination, Flight_date, Days_l
     prd = append(prd, prd_n)
   }
 }
-
 
 
 # Define UI ----
@@ -81,41 +80,42 @@ ui <- fluidPage(
                        choices = list("After 6 PM","Before 6 PM", "6 AM - 12 PM","12 PM - 6 PM"
                        ), selected = 1)),
     column(5, 
-             dateInput("date", 
-                       (h3("Date input")), 
-                       value = today())   
+           dateInput("date", h3("Select Date"),
+             min = as.Date(today()),
+             max = NULL,
+           )
     ),
     
              column(10,
                      submitButton("Submit"), align = "center"),
     mainPanel(
-      textOutput("arrc"),
-      textOutput("depc"), 
+    #   textOutput("arrc"),
+    #   textOutput("depc"), 
       textOutput("date"),
-      textOutput("stops"),
-      textOutput("airline"),
-      textOutput("class"),
-      textOutput("deptime"),
-      textOutput("arrtime")
+    #   textOutput("stops"),
+    #   textOutput("airline"),
+    #   textOutput("class"),
+    #   textOutput("deptime"),
+    #   textOutput("arrtime")
     )
   )
 )
   
 # Define server logic ----
 server <- function(input, output) {
-  output$arrc <- renderText({paste("You have selected the arrival city of", input$arrc)})
-  output$depc <- renderText({paste("with departue city as", input$depc)})
-  output$date <- renderText({paste("with days left to travel of", as.POSIXlt(as.Date(input$date, origin = "13-01-2022")) - today())})
-  output$stops <- renderText({paste("with stops:", input$stops)})
-  output$airline <- renderText({paste("in", input$airline)})
-  output$class <- renderText({paste("in class", input$class)})
-  output$deptime <- renderText({paste("leaving at", input$deptime)})
-  output$arrtime <- renderText({paste("arriving at", input$arrtime)})
-  date_inp <- reactive({as.POSIXlt(as.Date(input$date, origin = "13-01-2023"))})
-  aaj <- reactive({today()})
-  days_left <- reactive({as.POSIXlt(as.Date(input$date, origin = "13-01-2023")) - today()})
-  pred = flightPricePredict(input$depc, input$arrc, date_inp, days_left, input$class,  input$stops, input$deptime,input$arrtime)
-  output$prediction <- renderText({paste("the price is cheapest at", pred)}) 
+#  output$arrc <- renderText({paste("You have selected the arrival city of", input$arrc)})
+#  output$depc <- renderText({paste("with departue city as", input$depc)})
+   # output$date <- renderText({paste("with days left to travel of", (as.Date(input$date) - today()))})
+#  output$stops <- renderText({paste("with stops:", input$stops)})
+#  output$airline <- renderText({paste("in", input$airline)})
+#  output$class <- renderText({paste("in class", input$class)})
+#  output$deptime <- renderText({paste("leaving at", input$deptime)})
+#  output$arrtime <- renderText({paste("arriving at", input$arrtime)})
+   date_inp <- reactive({as.Date(input$date)})
+   aaj <- reactive({today()})
+   days_left <- reactive({as.Date(input$date) - today()})
+   pred = flightPricePredict(input$depc, input$arrc, date_inp, days_left, input$class,  input$stops, input$deptime,input$arrtime)
+   output$prediction <- renderText({paste("the price is cheapest at", pred)})
 }
 
 # Run the app ----
